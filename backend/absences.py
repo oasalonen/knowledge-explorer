cbe018057c30f46dba72dfa40070b41192fb5025
Olli Salonen <olli.salonen@futurice.com>
2017-09-25 09:41:50 +0200

    Refactoring.

--- backend/absences.py
+++ backend/absences.py
@@ -2,13 +2,32 @@ import csv
 import json
 import time
 
+DATA_DIR = "data/"
+
+# Data format of absence CSV file
+COLUMN_PERSON = 4
+COLUMN_TYPE = 7
+COLUMN_START = 11
+COLUMN_END = 12
+
+# Keys for KES JSON data
+KEY_PERSON = "person"
+KEY_TYPE = "type"
+KEY_START = "start"
+KEY_END = "end"
+KEY_FIRST_NAME = "first_name"
+KEY_LAST_NAME = "last_name"
+
+def data_path(filename):
+    return DATA_DIR + filename
+
 def convert_date(datetime_string):
     dt = time.strptime(datetime_string, "%d.%m.%Y %H:%M")
     return time.strftime("%Y-%m-%d", dt)
 
 def convert_person(name_string):
     names = name_string.split(", ")
-    return {"first_name": names[1], "last_name": names[0]}
+    return {KEY_FIRST_NAME: names[1], KEY_LAST_NAME: names[0]}
 
 def synonyms(base):
     return [base, base.upper(), base.lower()]
@@ -18,35 +37,38 @@ def synonym_data(items):
     single_synonyms = [[i[0], j] for i in expanded_synonyms for j in i[1:]]
     return "\n".join([json.dumps(i) for i in single_synonyms])
 
-with open('absences.csv', 'r') as f:
+with open(data_path('absences.csv'), 'r') as f:
     reader  = csv.reader(f, delimiter=';')
     raw_absences = list(reader)
 
+# Preprocess input data
 absences = raw_absences[1:]
-absences = list(map(lambda a: {"person": convert_person(a[4]),
-                               "type": a[7],
-                               "start": convert_date(a[11]),
-                               "end": convert_date(a[12])}, absences))
+absences = list(map(lambda a: {KEY_PERSON: convert_person(a[COLUMN_PERSON]),
+                               KEY_TYPE: a[COLUMN_TYPE],
+                               KEY_START: convert_date(a[COLUMN_START]),
+                               KEY_END: convert_date(a[COLUMN_END])},
+                    absences))
 data = "\n".join(list(map(lambda a: json.dumps(a), absences)))
 
-first_names = set([a["person"]["first_name"] for a in absences])
-last_names = set([a["person"]["last_name"] for a in absences])
-absence_types = set([a["type"] for a in absences])
+first_names = set([a[KEY_PERSON][KEY_FIRST_NAME] for a in absences])
+last_names = set([a[KEY_PERSON][KEY_LAST_NAME] for a in absences])
+absence_types = set([a[KEY_TYPE] for a in absences])
 
+# Create synonyms for case insensitive search
 first_name_syns = synonym_data(first_names)
 last_name_syns = synonym_data(last_names)
 absence_type_syns = synonym_data(absence_types)
 
-with open('data.json', 'w') as f:
+with open(data_path('data.json'), 'w') as f:
     f.write(data)
-with open('first_name.syn', 'w') as f:
+with open(data_path('first_name.syn'), 'w') as f:
     f.write(first_name_syns)
-with open('last_name.syn', 'w') as f:
+with open(data_path('last_name.syn'), 'w') as f:
     f.write(last_name_syns)
-with open('type.syn', 'w') as f:
+with open(data_path('type.syn'), 'w') as f:
     f.write(absence_type_syns)
 
-print(json)
-print(first_name_syns)
-print(last_name_syns)
-print(absence_type_syns)
+#print(json)
+#print(first_name_syns)
+#print(last_name_syns)
+#print(absence_type_syns)
--- backend/run-all.ps1
+++ backend/run-all.ps1
@@ -1,6 +1,6 @@
-iex 'rm .\absences.index'
-iex 'rm .\absences.grammar'
+iex 'rm data\absences.index'
+iex 'rm data\absences.grammar'
 
-iex 'kes.exe build_index schema.json data.json absences.index'
-iex 'kes.exe build_grammar grammar.xml absences.grammar'
-iex 'kes.exe host_service absences.grammar absences.index'
+iex 'kes.exe build_index schema.json data\data.json data\absences.index'
+iex 'kes.exe build_grammar grammar.xml data\absences.grammar'
+iex 'kes.exe host_service data\absences.grammar data\absences.index'
